e83b5510861b6cd228397217ca09358d27043440
Giuseppe Sorce <giuseppe.sorce@kreuzwerker.de>
2014-01-09 19:01:00 +0100

    first test integration attempt

---
+++ test/hello-world-test.js
@@ -0,0 +1,11 @@
+var App = require('application');
+    // chai = require('chai'),
+    // assert = chai.assert;
+
+describe('Hello World', function () {
+    it('should run this test', function(done) {
+      console.log(App);
+      // assert(true);
+      done();
+    })
+});
